7b3adb757c9736045be4e9970d1ec32a3e6ae9fe
Adrian.Conlon <adrian.conlon@gmail.com>
2017-09-12 13:38:30 +0100

    Rearrange ROM layout
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- src/Board.cpp
+++ src/Board.cpp
@@ -15,77 +15,83 @@ void Board::initialise() {
 
 	loadBootRom(romDirectory + "/DMG_ROM.bin");
 
-	//loadGameRom(romDirectory + "/Tetris (World).gb");
+	//loadGameRom(romDirectory + "/games/Tetris (World).gb");
+	//loadGameRom(romDirectory + "/games/Dr. Mario (V1.0) (JU) [!].gb");
+	//loadGameRom(romDirectory + "/games/Kirby's Dream Land (U) [!].gb");
 
-	loadGameRom(romDirectory + "/cpu_instrs.gb");				// Passed
-	//loadGameRom(romDirectory + "/01-special.gb");				// Passed
-	//loadGameRom(romDirectory + "/02-interrupts.gb");			// Passed
-	//loadGameRom(romDirectory + "/03-op sp,hl.gb");				// Passed
-	//loadGameRom(romDirectory + "/04-op r,imm.gb");				// Passed
-	//loadGameRom(romDirectory + "/05-op rp.gb");					// Passed
-	//loadGameRom(romDirectory + "/06-ld r,r.gb");				// Passed
-	//loadGameRom(romDirectory + "/07-jr,jp,call,ret,rst.gb");	// Passed
-	//loadGameRom(romDirectory + "/08-misc instrs.gb");			// Passed
-	//loadGameRom(romDirectory + "/09-op r,r.gb");				// Passed
-	//loadGameRom(romDirectory + "/10-bit ops.gb");				// Passed
-	//loadGameRom(romDirectory + "/11-op a,(hl).gb");				// Passed
+	//loadGameRom(romDirectory + "/games/opus5.gb");
+	//loadGameRom(romDirectory + "/games/ttt.gb");
 
-	//loadGameRom(romDirectory + "/instr_timing.gb");				// Failed #255
-	//loadGameRom(romDirectory + "/interrupt_time.gb");			// Failed
+	//loadGameRom(romDirectory + "/blargg/cpu_instrs.gb");				// Passed
+	//loadGameRom(romDirectory + "/blargg/01-special.gb");				// Passed
+	//loadGameRom(romDirectory + "/blargg/02-interrupts.gb");			// Passed
+	//loadGameRom(romDirectory + "/blargg/03-op sp,hl.gb");				// Passed
+	//loadGameRom(romDirectory + "/blargg/04-op r,imm.gb");				// Passed
+	//loadGameRom(romDirectory + "/blargg/05-op rp.gb");					// Passed
+	//loadGameRom(romDirectory + "/blargg/06-ld r,r.gb");				// Passed
+	//loadGameRom(romDirectory + "/blargg/07-jr,jp,call,ret,rst.gb");	// Passed
+	//loadGameRom(romDirectory + "/blargg/08-misc instrs.gb");			// Passed
+	//loadGameRom(romDirectory + "/blargg/09-op r,r.gb");				// Passed
+	//loadGameRom(romDirectory + "/blargg/10-bit ops.gb");				// Passed
+	//loadGameRom(romDirectory + "/blargg/11-op a,(hl).gb");				// Passed
 
-	//loadGameRom(romDirectory + "/opus5.gb");
-	//loadGameRom(romDirectory + "/ttt.gb");
+	//loadGameRom(romDirectory + "/blargg/instr_timing.gb");				// Failed #255
+	//loadGameRom(romDirectory + "/blargg/interrupt_time.gb");			// Failed
 
-	//loadGameRom(romDirectory + "/acceptance/add_sp_e_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_hwio-dmg0.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_hwio-dmgABCXmgb.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_hwio-S.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_regs-dmg0.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_regs-dmgABCX.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_regs-mgb.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_regs-sgb.gb");
-	//loadGameRom(romDirectory + "/acceptance/boot_regs-sgb2.gb");
-	//loadGameRom(romDirectory + "/acceptance/call_cc_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/call_cc_timing2.gb");
-	//loadGameRom(romDirectory + "/acceptance/call_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/call_timing2.gb");
-	//loadGameRom(romDirectory + "/acceptance/div_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/di_timing-GS.gb");
-	//loadGameRom(romDirectory + "/acceptance/ei_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/halt_ime0_ei.gb");
-	//loadGameRom(romDirectory + "/acceptance/halt_ime0_nointr_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/halt_ime1_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/halt_ime1_timing2-GS.gb");
-	//loadGameRom(romDirectory + "/acceptance/if_ie_registers.gb");
-	//loadGameRom(romDirectory + "/acceptance/intr_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/jp_cc_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/jp_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/ld_hl_sp_e_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/oam_dma_restart.gb");
-	//loadGameRom(romDirectory + "/acceptance/oam_dma_start.gb");
-	//loadGameRom(romDirectory + "/acceptance/oam_dma_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/pop_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/push_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/rapid_di_ei.gb");
-	//loadGameRom(romDirectory + "/acceptance/reti_intr_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/reti_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/ret_cc_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/ret_timing.gb");
-	//loadGameRom(romDirectory + "/acceptance/rst_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/add_sp_e_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_hwio-dmg0.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_hwio-dmgABCXmgb.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_hwio-S.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_regs-dmg0.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_regs-dmgABCX.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_regs-mgb.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_regs-sgb.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/boot_regs-sgb2.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/call_cc_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/call_cc_timing2.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/call_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/call_timing2.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/div_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/di_timing-GS.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/ei_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/halt_ime0_ei.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/halt_ime0_nointr_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/halt_ime1_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/halt_ime1_timing2-GS.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/if_ie_registers.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/intr_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/jp_cc_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/jp_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/ld_hl_sp_e_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/oam_dma_restart.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/oam_dma_start.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/oam_dma_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/pop_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/push_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/rapid_di_ei.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/reti_intr_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/reti_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/ret_cc_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/ret_timing.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/rst_timing.gb");
 
-	//loadGameRom(romDirectory + "/acceptance/timer/div_write.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/rapid_toggle.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim00.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim00_div_trigger.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim01.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim01_div_trigger.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim10.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim10_div_trigger.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim11.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tim11_div_trigger.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tima_reload.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tima_write_reloading.gb");
-	//loadGameRom(romDirectory + "/acceptance/timer/tma_write_reloading.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/bits/mem_oam.gb");	// Pass
+	//loadGameRom(romDirectory + "/mooneye/acceptance/bits/reg_f.gb");	// Pass
+	loadGameRom(romDirectory + "/mooneye/acceptance/bits/unused_hwio-GS.gb");
+
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/div_write.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/rapid_toggle.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim00.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim00_div_trigger.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim01.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim01_div_trigger.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim10.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim10_div_trigger.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim11.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tim11_div_trigger.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tima_reload.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tima_write_reloading.gb");
+	//loadGameRom(romDirectory + "/mooneye/acceptance/timer/tma_write_reloading.gb");
 
 	if (m_configuration.isProfileMode()) {
 		m_cpu.ExecutingInstruction.connect(std::bind(&Board::Cpu_ExecutingInstruction_Profile, this, std::placeholders::_1));
